ecdf0ab7e67fcc595ce00038c21032d003b4493e
Jo Geek <jonesvinothjoseph@hotmail.com>
2019-05-21 13:26:05 +0530

    Assessment page

--- assets/js/mango.js
+++ assets/js/mango.js
@@ -6,10 +6,17 @@
 
 $(document).ready(function () {
     dataActionUrl();
+    windowActionUrl();
 })
 
 var dataActionUrl = function () {
     $('body').on('click', '[data-action-url]', function () {
         location.href = $(this).data('action-url');
+    })
+}
+
+var windowActionUrl = function () {
+    $('body').on('click', '[data-window-url]', function () {
+        window.open($(this).data('window-url'), 'assessment', 'scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=1000,height=500');
     })
 }
